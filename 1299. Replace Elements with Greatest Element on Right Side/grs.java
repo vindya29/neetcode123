d7477319f66f67b7911c30c7f28c6033898ff53f
vindya <69504272+vindya29@users.noreply.github.com>
2024-06-27 11:28:43 -0500

    Create grs.java

---
+++ 1299. Replace Elements with Greatest Element on Right Side/grs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max=-1;
+        for(int i=arr.length-1;i>=0;i--){
+         
+            if(arr[i] > max){
+                int temp = max;
+                max= arr[i];
+                arr[i]=temp;
+            }
+        else{
+           arr[i] = max;
+        }
+        
+    }
+    return arr;
+    }
+}
